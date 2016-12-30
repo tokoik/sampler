ff90e2071f007567b651086026af9c0cb991708b
Kohe Tokoi <tokoik@gmail.com>
2016-12-30 15:14:53 +0900

    Initial Commit

---
+++ sampler/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  sampler
+//
+//  Created by こうへい on 2016/12/30.
+//  Copyright © 2016年 Kohe Tokoi. All rights reserved.
+//
+
+#include "glfw3.h"
+
+int main(int argc, const char * argv[])
+{
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
